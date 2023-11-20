0607c265f664563cfdafe80803ad8be74929e101
paul <leebosuk89@gmail.com>
2023-11-20 23:15:24 +0900

    23-11-20

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,13 +20,30 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
-        em.createQuery("select o from Order o join o.member m" +
+
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += "where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        //회원 이름 검색
+
+        /*em.createQuery("select o from Order o join o.member m" +
                 " where o.status = :status " +
                 " and m.name like :name", Order.class)
                 .setParameter("status", orderSearch.getOrderStatus())
                 .setParameter("name", orderSearch.getMemberName())
                 //.setFirstResult(100) //페이징 하는법인데 100부터 시작해서 가져온다는말
                 .setMaxResults(1000) //최대 1000건
-                .getResultList();//Order, 그리고 order와 연관된 멤버를 조인해
+                .getResultList();//Order, 그리고 order와 연관된 멤버를 조인해*/
     }
 }
