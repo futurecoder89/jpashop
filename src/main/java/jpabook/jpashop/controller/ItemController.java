af7a308be23473872b696cfcc5dc67b52fba45a6
paul <leebosuk89@gmail.com>
2023-11-21 20:38:47 +0900

    23-11-21

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public  String create(BookForm form){
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        //Order에서본것처럼 static 생성자 메소드를 가지고 의도에 맞게 설계하는게 좋다.
+        //set을 많이쓰는것은 좋은 설계가 아니다.
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+}
